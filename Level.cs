2cba58e462400b810f96f26a38bcb5c548a480c2
15rtrujillo <ryantrujillo@comcast.net>
2024-01-20 08:21:25 -0700

    Added a coupe.

--- Level.cs
+++ Level.cs
@@ -6,12 +6,20 @@ public partial class Level : Resource
 {
 	[Export]
 	public int LevelTime { get; set; }
+
 	[ExportGroup("Sedan")]
 	[Export]
 	public int SedanSpeed { get; set; }
 	[Export]
 	public float SedanSpawnInterval { get; set; }
-	[ExportGroup("Semi-Truck")]
+
+    [ExportGroup("Coupe")]
+    [Export]
+    public int CoupeSpeed { get; set; }
+    [Export]
+    public float CoupeSpawnInterval { get; set; }
+
+    [ExportGroup("Semi-Truck")]
 	[Export]
 	public int SemiTruckSpeed { get; set; }
 	[Export]
--- Main.cs
+++ Main.cs
@@ -4,31 +4,43 @@ using System;
 public partial class Main : Node
 {
 	[ExportGroup("Levels")]
-	[Export]
+    // Debug option for starting at a certain level. The levels are zero-indexed.
+    [Export]
+    public int BeginAtLevel { get; set; } = 0;
+    [Export]
 	public Godot.Collections.Array<Level> Levels { get; set; }
 	
-	// Debug option for starting at a certain level. The levels are zero-indexed.
-	[Export]
-	public int BeginAtLevel { get; set; } = 0;
-	
-	private Level _currentLevel;
+	private int _currentLevel;
 	private Player _player;
 	private PackedScene _sedanScene;
 	private PackedScene _semi_TruckScene;
-	
-	public override void _Ready()
+    private PackedScene _coupeScene;
+
+    public override void _Ready()
 	{
-		_currentLevel = Levels[BeginAtLevel];
 		_player = GetNode<Player>("Player");
 		_player.Position = GetNode<Marker2D>("SpawnLocation").Position;
 		
 		_sedanScene = ResourceLoader.Load<PackedScene>("res://entity/Sedan.tscn");
 		_semi_TruckScene = ResourceLoader.Load<PackedScene>("res://entity/Semi_Truck.tscn");
+		_coupeScene = ResourceLoader.Load<PackedScene>("res://entity/Coupe.tscn");
+
+		StartLevel();
 	}
 	
 	private void StartLevel()
 	{
-		
+		Timer truckSpawnTimer = GetNode<Timer>("Semi_TruckSpawnTimer");
+		truckSpawnTimer.WaitTime = Levels[_currentLevel].SemiTruckSpawnInterval;
+		truckSpawnTimer.Start();
+
+		Timer sedanSpawnTimer = GetNode<Timer>("SedanSpawnTimer");
+		sedanSpawnTimer.WaitTime = Levels[_currentLevel].SedanSpawnInterval;
+        sedanSpawnTimer.Start();
+
+		Timer coupeSpawnTimer = GetNode<Timer>("CoupeSpawnTimer");
+		coupeSpawnTimer.WaitTime = Levels[_currentLevel].CoupeSpawnInterval;
+		coupeSpawnTimer.Start();
 	}
 	
 	private void InitializeEnemy(Enemy enemy, int speed, Vector2 direction)
@@ -43,12 +55,18 @@ public partial class Main : Node
 	private void OnSedanSpawnTimerTimeout()
 	{
 		Enemy sedan = _sedanScene.Instantiate<Enemy>();
-		InitializeEnemy(sedan, _currentLevel.SedanSpeed, Vector2.Left);
+		InitializeEnemy(sedan, Levels[_currentLevel].SedanSpeed, Vector2.Left);
 	}
 	
 	private void OnSemi_TruckSpawnTimerTimeout()
 	{
 		Enemy semi_Truck = _semi_TruckScene.Instantiate<Enemy>();
-		InitializeEnemy(semi_Truck, _currentLevel.SemiTruckSpeed, Vector2.Right);
+		InitializeEnemy(semi_Truck, Levels[_currentLevel].SemiTruckSpeed, Vector2.Right);
+	}
+
+	private void OnCoupeSpawnTimerTimeout()
+	{
+		Enemy coupe = _coupeScene.Instantiate<Enemy>();
+		InitializeEnemy(coupe, Levels[_currentLevel].CoupeSpeed, Vector2.Right);
 	}
 }
